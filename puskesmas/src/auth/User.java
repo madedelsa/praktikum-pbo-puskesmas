cb4ece6437394df635d8a4ee611e7b046174258c
naawra <nrhrifdah@gmail.com>
2025-05-15 10:17:56 +0700

    k

--- puskesmas/src/auth/User.java
+++ puskesmas/src/auth/User.java
@@ -6,7 +6,7 @@ public abstract class User {
 
     
     public static void main(String[] args) {
-        System.out.println("TESsfadfgyg");
+        System.out.println("TESsfadfgyg");jj
     }
     
 }
